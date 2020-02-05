e37beae8a72612f630f0eb23462e39e7e1976db3
James <james.s.shoaf@gmail.com>
2020-02-05 12:22:43 -0800

    implement stack and update queue for functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,22 +7,22 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    keys = Object.keys(storage);
-    if (keys.length !== 0){
-      for (let i = 0; i < keys.length; i++) {
-        storage[keys.length - i] = storage[keys.length - i -1];
+    var length = Object.keys(storage).length;
+    if (length !== 0) {
+      for (let i = 0; i < length; i++) {
+        storage[length - i] = storage[length - i -1];
       }
     }
     storage[0] = value;
   };
 
   someInstance.dequeue = function() {
-    keys = Object.keys(storage);
-    if (keys.length === 0) {
+    var length = Object.keys(storage).length;
+    if (length === 0) {
       return undefined;
     }
-    var lastOut = storage[keys.length-1];
-    delete storage[keys.length-1];
+    var lastOut = storage[length-1];
+    delete storage[length-1];
     return lastOut;
   };
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,22 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var length = Object.keys(storage).length;
+    storage[length] = value;
   };
 
   someInstance.pop = function() {
+    var length = Object.keys(storage).length;
+    if (length === 0){
+      return undefined;
+    }
+    var lastOut = storage[length -1];
+    delete storage[length-1];
+    return lastOut;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
